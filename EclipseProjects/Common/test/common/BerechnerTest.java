96e69632a506ffeec9057806ea12b2df8885ac7f
leider <derleider@web.de>
2012-05-06 10:47:44 +0200

    added an additional test

--- EclipseProjects/Common/test/common/BerechnerTest.java
+++ EclipseProjects/Common/test/common/BerechnerTest.java
@@ -12,6 +12,7 @@ public abstract class BerechnerTest
 {
 	private static final LocalDate APRIL_ULTIMO = new LocalDate(2010, 4, 30);
 	private static final LocalDate MAI_ULTIMO = new LocalDate(2010, 5, 31);
+	private static final LocalDate JUNI_ULTIMO = new LocalDate(2010, 6, 30);
 	private final List<Monatsdaten> monatsdaten = new ArrayList<Monatsdaten>();
 
 	protected final List<Umsatz> umsaetze = new ArrayList<Umsatz>();
@@ -60,5 +61,21 @@ public abstract class BerechnerTest
 		assertEquals(50, aprilData.getDurchschnittsBestand());
 		assertEquals(300, maiData.getBestand());
 		assertEquals(203, maiData.getDurchschnittsBestand());
+	}
+
+	@Test
+	public void vormonatsBestandWirdUeberZweiMonateMitberuecksichtigt()
+	{
+		umsaetze.add(new Umsatz(new LocalDate(2010, 4, 16), 100));
+		umsaetze.add(new Umsatz(new LocalDate(2010, 5, 16), 200));
+		monatsdaten.add(new Monatsdaten(APRIL_ULTIMO));
+		monatsdaten.add(new Monatsdaten(MAI_ULTIMO));
+		monatsdaten.add(new Monatsdaten(JUNI_ULTIMO));
+
+		berechner.fillData(monatsdaten);
+
+		Monatsdaten juniData = monatsdaten.get(2);
+		assertEquals(300, juniData.getBestand());
+		assertEquals(300, juniData.getDurchschnittsBestand());
 	}
 }
