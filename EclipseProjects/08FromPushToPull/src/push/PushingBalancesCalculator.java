e4d1665ab91c1f65797966f16330b95714a8d9e5
Nicole Rauch <nicole.m@gmx.de>
2013-11-17 16:20:25 +0100

    Grouping of steps 8 and 9 - part II (addition of new step 8)

---
+++ EclipseProjects/08FromPushToPull/src/push/PushingBalancesCalculator.java
@@ -0,0 +1,61 @@
+package push;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.LocalDate;
+
+import common.BalancesOfMonth;
+import common.BalancesOfMonthCalculator;
+import common.Transaction;
+
+public class PushingBalancesCalculator implements BalancesOfMonthCalculator
+{
+
+	private final List<Transaction> transactions;
+
+	public PushingBalancesCalculator(List<Transaction> transactions)
+	{
+		super();
+		this.transactions = transactions;
+	}
+
+	@Override
+	public void fillData(List<BalancesOfMonth> balancesOfMonthList)
+	{
+		BalanceAndAverage balanceAndAverage = new BalanceAndAverage();
+
+		for (BalancesOfMonth balancesOfMonth : balancesOfMonthList)
+		{
+			LocalDate dateOfMonth = balancesOfMonth.getDate();
+			List<Transaction> transactionsOfMonth = transactionsOfMonth(dateOfMonth);
+
+			int precedingBalance = balanceAndAverage.getBalance();
+
+			balanceAndAverage = new BalanceAndAverage(dateOfMonth, transactionsOfMonth, precedingBalance);
+
+			balancesOfMonth.setBalance(balanceAndAverage.getBalance());
+			balancesOfMonth.setAverageBalance(balanceAndAverage.getAverageBalance());
+		}
+	}
+
+	private List<Transaction> transactionsOfMonth(LocalDate date)
+	{
+		List<Transaction> results = new ArrayList<Transaction>();
+		for (Transaction transaction : transactions)
+		{
+			LocalDate dateOfTransaction = transaction.getDate();
+			if (areSameMonthAndYear(date, dateOfTransaction))
+			{
+				results.add(transaction);
+			}
+		}
+		return results;
+	}
+
+	private boolean areSameMonthAndYear(LocalDate date, LocalDate dateOfTransaction)
+	{
+		return dateOfTransaction.getMonthOfYear() == date.getMonthOfYear() && dateOfTransaction.getYear() == date.getYear();
+	}
+
+}
---
+++ EclipseProjects/08FromPushToPull/test/push/PushTest.java
@@ -0,0 +1,16 @@
+package push;
+
+import org.junit.Before;
+
+import common.CalculatorTest;
+
+public class PushTest extends CalculatorTest
+{
+
+	@Before
+	public void setup()
+	{
+		calculator = new PushingBalancesCalculator(transactions);
+	}
+
+}
