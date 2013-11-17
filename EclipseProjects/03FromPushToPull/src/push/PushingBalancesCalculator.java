3cb5117d4b892fc8695d1c3e68cf8f98b16ec68e
Nicole Rauch <nicole.m@gmx.de>
2013-11-17 19:06:49 +0100

    Improving multi-months, part IV (added new step 3 that extracts the method, fixes the parameter-write problem)

---
+++ EclipseProjects/03FromPushToPull/src/push/PushingBalancesCalculator.java
@@ -0,0 +1,92 @@
+package push;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.LocalDate;
+
+import common.BalancesOfMonth;
+import common.BalancesOfMonthCalculator;
+import common.Transaction;
+
+public class PushingBalancesCalculator implements BalancesOfMonthCalculator
+{
+
+	private final List<Transaction> transactions;
+
+	public PushingBalancesCalculator(List<Transaction> transactions)
+	{
+		super();
+		this.transactions = transactions;
+	}
+
+	@Override
+	public void fillData(List<BalancesOfMonth> balancesOfMonthList)
+	{
+		ValuesOfMonth valuesOfMonth = new ValuesOfMonth();
+		int balance = 0;
+
+		for (BalancesOfMonth balancesOfMonth : balancesOfMonthList)
+		{
+			LocalDate dateOfMonth = balancesOfMonth.getDate();
+			List<Transaction> transactionsOfMonth = transactionsOfMonth(dateOfMonth);
+
+			balance = calculateValues(valuesOfMonth, balance, dateOfMonth, transactionsOfMonth);
+
+			balancesOfMonth.setBalance(valuesOfMonth.getBalance());
+			balancesOfMonth.setAverageBalance(valuesOfMonth.getAverageBalance());
+		}
+	}
+
+	private int calculateValues(ValuesOfMonth valuesOfMonth, int previousBalance, LocalDate dateOfMonth, List<Transaction> transactionsOfMonth)
+	{
+		int ultimo = dateOfMonth.getDayOfMonth();
+		int balance = previousBalance;
+
+		double averageBalance = 0;
+		int dayOfLatestBalance = 1;
+		for (Transaction transaction : transactionsOfMonth)
+		{
+			int day = transaction.getDate().getDayOfMonth();
+			averageBalance += calculateProportionalBalance(dayOfLatestBalance, balance, day, ultimo);
+			balance += transaction.getAmount();
+			dayOfLatestBalance = day;
+		}
+
+		averageBalance += calculateProportionalBalance(dayOfLatestBalance, balance, ultimo + 1, ultimo);
+
+		valuesOfMonth.setBalanceAndAverage(balance, averageBalance);
+		return balance;
+	}
+
+	private double calculateProportionalBalance(int dayOfLatestBalance, int balance, int day, int daysInMonth)
+	{
+		int countingDays = day - dayOfLatestBalance;
+		if (countingDays == 0)
+		{
+			return 0;
+		}
+		double rate = (double) countingDays / daysInMonth;
+		return (balance * rate);
+	}
+
+	private List<Transaction> transactionsOfMonth(LocalDate date)
+	{
+		List<Transaction> results = new ArrayList<Transaction>();
+		for (Transaction transaction : transactions)
+		{
+			LocalDate dateOfTransaction = transaction.getDate();
+			if (areSameMonthAndYear(date, dateOfTransaction))
+			{
+				results.add(transaction);
+			}
+		}
+		return results;
+	}
+
+	private boolean areSameMonthAndYear(LocalDate date, LocalDate dateOfTransaction)
+	{
+		return dateOfTransaction.getMonthOfYear() == date.getMonthOfYear() && dateOfTransaction.getYear() == date.getYear();
+	}
+
+}
