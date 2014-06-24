093784f2194248828d04ab26045ac21c377be4fd
Samir Talwar <samir@noodlesandwich.com>
2014-06-25 00:23:37 +0100

    Step #6.

---
+++ csharp/06_FromPushToPull/PushingBalancesCalculator.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using Common;
+
+namespace FromPushToPull
+{
+    public class PushingBalancesCalculator : BalancesOfMonthCalculator
+    {
+        private readonly IList<Transaction> transactions;
+
+        public PushingBalancesCalculator(IList<Transaction> transactions)
+        {
+            this.transactions = transactions;
+        }
+
+        public void FillData(IList<BalancesOfMonth> balancesOfMonthList)
+        {
+            ValuesOfMonth valuesOfMonth = new ValuesOfMonth();
+
+            foreach (BalancesOfMonth balancesOfMonth in balancesOfMonthList)
+            {
+                DateTime dateOfMonth = balancesOfMonth.Date;
+                IList<Transaction> transactionsOfMonth = TransactionsOfMonth(dateOfMonth);
+
+                int precedingBalance = valuesOfMonth.Balance;
+
+                valuesOfMonth = new ValuesOfMonth();
+                valuesOfMonth.CalculateValues(dateOfMonth, transactionsOfMonth, precedingBalance);
+
+                balancesOfMonth.Balance = valuesOfMonth.Balance;
+                balancesOfMonth.AverageBalance = valuesOfMonth.AverageBalance;
+            }
+        }
+
+        private IList<Transaction> TransactionsOfMonth(DateTime date)
+        {
+            IList<Transaction> results = new List<Transaction>();
+            foreach (Transaction transaction in transactions)
+            {
+                DateTime dateOfTransaction = transaction.Date;
+                if (AreSameMonthAndYear(date, dateOfTransaction))
+                {
+                    results.Add(transaction);
+                }
+            }
+            return results;
+        }
+
+        private bool AreSameMonthAndYear(DateTime date, DateTime dateOfTransaction)
+        {
+            return dateOfTransaction.Month == date.Month && dateOfTransaction.Year == date.Year;
+        }
+
+    }
+}
