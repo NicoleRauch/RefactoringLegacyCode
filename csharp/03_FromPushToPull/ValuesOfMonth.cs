72d7c5f889de018ad1237c5a59377aff2321b64e
Samir Talwar <samir@noodlesandwich.com>
2014-06-24 21:24:34 +0100

    Step #3.

---
+++ csharp/03_FromPushToPull/ValuesOfMonth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FromPushToPull
+{
+    class ValuesOfMonth
+    {
+        public int Balance { get; set; }
+
+        public int AverageBalance { get; set; }
+    }
+}
