6a4a751a540e3cc881706b035519397ea8f1d628
Nicole Rauch <nicole.m@gmx.de>
2017-03-28 10:45:14 +0200

    Added step 09_5: Introduced a Months object

---
+++ 09_5FromPushToPull/src/push/Months.java
@@ -0,0 +1,32 @@
+package push;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.joda.time.LocalDate;
+
+import common.BalancesOfMonth_API;
+import common.Transaction_API;
+
+public class Months {
+
+	private final Map<LocalDate, ValuesOfMonth> months = new HashMap<LocalDate, ValuesOfMonth>();
+
+	public Months(List<BalancesOfMonth_API> balancesOfMonthList, Transactions transactions) {
+		ValuesOfMonth valuesOfMonth = null;
+		for (BalancesOfMonth_API balancesOfMonth : balancesOfMonthList) {
+			LocalDate dateOfMonth = balancesOfMonth.getDate();
+			List<Transaction_API> transactionsOfMonth = transactions.transactionsOfMonth(dateOfMonth);
+
+			valuesOfMonth = new ValuesOfMonth(valuesOfMonth == null ? new InitialValuesOfMonth() : valuesOfMonth, dateOfMonth,
+					transactionsOfMonth);
+			months.put(dateOfMonth, valuesOfMonth);
+		}
+	}
+
+	public ValuesOfMonth monthFor(LocalDate dateOfMonth) {
+		return months.get(dateOfMonth);
+	}
+
+}
---
+++ 09_5FromPushToPull/src/push/PullingBalancesCalculator.java
@@ -0,0 +1,27 @@
+package push;
+
+import java.util.List;
+
+import common.BalancesOfMonthCalculator_API;
+import common.BalancesOfMonth_API;
+import common.Transaction_API;
+
+public class PullingBalancesCalculator implements BalancesOfMonthCalculator_API {
+
+	private final Transactions transactions;
+
+	public PullingBalancesCalculator(List<Transaction_API> transactions) {
+		this.transactions = new Transactions(transactions);
+	}
+
+	@Override
+	public void fillData(List<BalancesOfMonth_API> balancesOfMonthList) {
+		Months months = new Months(balancesOfMonthList, transactions);
+
+		for (BalancesOfMonth_API balancesOfMonth : balancesOfMonthList) {
+			ValuesOfMonth currentMonth = months.monthFor(balancesOfMonth.getDate());
+			balancesOfMonth.setBalance(currentMonth.getBalance());
+			balancesOfMonth.setAverageBalance(currentMonth.getAverageBalance());
+		}
+	}
+}
